a34d5cbfdb62d737ff93d975251ba0258bb248a5
guoan <1naouguh@gmail.com>
2022-01-29 22:30:59 +0800

    234 只有一个节点的时候没有处理正确，要考虑这种异常情况

--- src/main/java/com/nou/lc/link/LC83.java
+++ src/main/java/com/nou/lc/link/LC83.java
@@ -5,33 +5,22 @@ import com.nou.lc.ListNode;
 /**
  * 删除排序链表中的重复元素
  * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
- * 思路: 链表遍历 ,
+ * 思路: 链表遍历 , 下面是比较精简的写法,通过循环控制变量移动，
  */
 public class LC83 {
 
     public ListNode deleteDuplicates(ListNode head) {
 
-        if(head==null||head.next==null){
-            return head;
-        }
+        ListNode currentNode = head;
+        while (currentNode!=null && currentNode.next!=null){
 
-
-        ListNode newLinkList = head;
-
-        ListNode tail = head;
-
-        // 遍历列表 , 将不存在的插入到新链表的尾部
-        ListNode current = head.next;
-        while (current!=null) {
-
-            if(tail.val!=current.val){
-                tail.next = current;
-                tail = tail.next;
+            if(currentNode.val==currentNode.next.val){
+                currentNode.next = currentNode.next.next;
+            }else{
+                currentNode = currentNode.next;
             }
-            current = current.next;
 
         }
-        tail.next = null;
-        return newLinkList;
+        return head;
     }
 }
