545890872d24bd0eae736d1b9ec1633ce043c889
guoan <1naouguh@gmail.com>
2022-01-29 16:14:08 +0800

    234 只有一个节点的时候没有处理正确，要考虑这种异常情况

---
+++ src/main/java/com/nou/lc/link/LC148.java
@@ -0,0 +1,18 @@
+package com.nou.lc.link;
+
+import com.nou.lc.ListNode;
+
+/**
+ * 排序链表
+ * https://leetcode.com/problems/sort-list/
+ * 解题思路 : 归并排序递归解法 ,先拆成两部分，然后进行合并 , 但是这个是 o(n) 的空间，
+ * 所以需要使用 bottom up  的做法.
+ */
+public class LC148 {
+
+    public ListNode sortList(ListNode head) {
+
+
+        return null ;
+    }
+}
