5f3519eab557baf236ee05f62be477b8905ddfa9
guoan <1naouguh@gmail.com>
2022-02-20 09:05:42 +0800

    sliding window

---
+++ src/main/java/com/nou/lc/queue/LC239.java
@@ -0,0 +1,43 @@
+package com.nou.lc.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 求滑动窗口的最大值.
+ * leetcode 239 : https://chuangleetcode-cn.com/problems/sliding-window-maximum/
+ * 思路 : 单调队列
+ */
+public class LC239 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        if(nums.length==0||k==0){
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+
+        Deque<Integer> windows = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+
+            // 这个是做什么,确保窗口大小为 k , 以 i 结尾的滑动窗口的起点是 i-k+1
+
+            while (windows.size()>0 && windows.peekFirst()<=i-k){
+                windows.pollFirst();
+            }
+
+            // 将前面比较小的排除掉 .
+            while (windows.size()>0 && nums[windows.peekLast()]<nums[i]){
+                windows.pollLast();
+            }
+
+            windows.offerLast(i);
+            if(i>=k-1){
+                result[i-k+1]= nums[windows.peekFirst()];
+            }
+        }
+        return result;
+    }
+
+}
--- src/main/java/com/nou/lc/tree/LC236.java
+++ src/main/java/com/nou/lc/tree/LC236.java
@@ -1,7 +1,26 @@
 package com.nou.lc.tree;
 
+import com.nou.lc.base.TreeNode;
+
 /**
- *
+ * 最近公共祖先
  */
 public class LC236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+
+        if(root==null||root==p||root==q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left!=null && right!=null){
+            return root;
+        }
+        if(left!=null){
+            return left;
+        }
+        return right;
+    }
+
 }
