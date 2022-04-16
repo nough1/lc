d8b0a5a47f416ad4ecfffdd811bd845ad25a55bb
guoan <1naouguh@gmail.com>
2022-04-16 14:47:33 +0800

    20220305

--- src/main/java/com/nou/lc/twopointers/LC283.java
+++ src/main/java/com/nou/lc/twopointers/LC283.java
@@ -21,4 +21,21 @@ public class LC283 {
             }
         }
     }
+
+    // 这种解法比较好理解
+    public void moveZeroes1(int[] nums) {
+
+       int left = 0 ;
+       for(int num:nums){
+
+           if(num!=0){
+               nums[left++] = num;
+           }
+
+       }
+
+       while (left<nums.length)nums[left++]=0;
+
+    }
+
 }
