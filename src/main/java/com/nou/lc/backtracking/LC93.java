34dada93fb989c2f79c740f1ae272b5d9911579f
guoan <1naouguh@gmail.com>
2022-05-19 08:02:15 +0800

    leetcode 179

--- src/main/java/com/nou/lc/backtracking/LC93.java
+++ src/main/java/com/nou/lc/backtracking/LC93.java
@@ -1,14 +1,21 @@
 package com.nou.lc.backtracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * https://www.youtube.com/watch?v=61tN4YEdiTM
  * https://leetcode-cn.com/problems/restore-ip-addresses/
+ *
  */
 public class LC93 {
 
     public List<String> restoreIpAddresses(String s) {
+
+        List<String> result = new ArrayList<>();
+        if(s==null ||s.length()==0||s.length()>12){
+            return result;
+        }
 
         return  null ;
     }
