519a2842f9e74805bcc3186cec4c405b144db942
guoan <1naouguh@gmail.com>
2022-06-15 21:34:13 +0800

    leetcode 179

---
+++ src/main/java/com/nou/lc/binarysearch/LC33.java
@@ -0,0 +1,8 @@
+package com.nou.lc.binarysearch;
+
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ *
+ */
+public class LC33 {
+}
--- src/main/java/com/nou/lc/stack/LC32.java
+++ src/main/java/com/nou/lc/stack/LC32.java
@@ -1,12 +1,41 @@
 package com.nou.lc.stack;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/longest-valid-parentheses/
+ * https://www.acwing.com/video/1353/
  */
 public class LC32 {
 
+
+    // 用到合法括号的两个性质，左右括号数量相等，任意前缀左括号大于右括号 .
     public int longestValidParentheses(String s) {
 
-        return 0;
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        int segStart = -1;
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }else{
+
+
+                if(stack.isEmpty()){
+                    segStart = i;
+                }else{
+
+                    stack.pop();
+                    if(stack.isEmpty()){
+                        result = Math.max(result,i-segStart);
+                    }else{
+                        result = Math.max(result,i-stack.peek());
+                    }
+                }
+
+            }
+        }
+        return result;
     }
 }
