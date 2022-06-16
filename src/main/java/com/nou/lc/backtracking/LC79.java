a7d3596b7015a32c5c50faf88c0cca7664a99c6f
guoanhu <8naouguh@gmail.com>
2022-06-16 23:35:05 +0800

    leetcode 179

---
+++ src/main/java/com/nou/lc/backtracking/LC79.java
@@ -0,0 +1,13 @@
+package com.nou.lc.backtracking;
+
+/**
+ * https://leetcode.com/problems/word-search/
+ *
+ */
+public class LC79 {
+
+    public boolean exist(char[][] board, String word) {
+
+        return false;
+    }
+}
