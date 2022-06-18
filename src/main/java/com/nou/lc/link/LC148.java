6a5cbdb16379be2df82ba3f848f1395c7fc01fc2
guoanhu <8naouguh@gmail.com>
2022-06-18 16:25:12 +0800

    leetcode 179

--- src/main/java/com/nou/lc/link/LC148.java
+++ src/main/java/com/nou/lc/link/LC148.java
@@ -12,7 +12,64 @@ public class LC148 {
 
     public ListNode sortList(ListNode head) {
 
+        if(head==null||head.next==null){
+            return head;
+        }
 
-        return null ;
+        // split two part
+        ListNode left = head;
+        ListNode right = getMid(head);
+        ListNode temp = right.next;
+        right.next=null;
+        right = temp;
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left,right) ;
+    }
+
+    public ListNode merge(ListNode left,ListNode right){
+
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while (left!=null && right!=null){
+
+            if(left.val< right.val){
+
+                tail.next = left;
+                left = left.next;
+
+            }else{
+
+                tail.next = right;
+                right = right.next;
+
+            }
+            tail = tail.next;
+        }
+
+        if(left!=null){
+
+            tail.next = left;
+
+        }
+
+        if(right!=null){
+            tail.next = right;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode getMid(ListNode head){
+
+        ListNode slow = head,fast = slow.next;
+
+        while (fast!=null && fast.next!=null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
