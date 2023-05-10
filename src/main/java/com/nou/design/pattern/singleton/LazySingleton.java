df9c3f455dab3c5a2ea68f886787dcc16d1544c9
guoan <1naouguh@gmail.com>
2023-05-10 17:08:51 +0800

    add lazy init

---
+++ src/main/java/com/nou/design/pattern/singleton/LazySingleton.java
@@ -0,0 +1,32 @@
+package com.nou.design.pattern.singleton;
+
+public class LazySingleton {
+
+    private static LazySingleton singleton;
+
+    private LazySingleton(){
+
+
+    }
+
+    public static LazySingleton getInstance(){
+
+
+        if(singleton==null){
+
+            synchronized (LazySingleton.class){
+
+                if(singleton==null){
+
+                    singleton = new LazySingleton();
+
+                }
+
+            }
+
+        }
+
+        return singleton;
+    }
+
+}
